b15c4ef5359444e9dfc1e7da8dacb6d0ddcd900b
edcastillob <edwar.castillo@gmail.com>
2023-06-01 10:35:00 -0400

    Base-listar

--- 03.1-bases-Console/app.js
+++ 03.1-bases-Console/app.js
@@ -1,15 +1,35 @@
 const { crearArchivo } = require('./helpers/multiplicar')
-const argv = require('yargs').argv;
+const argv = require('yargs')
+            .option('b', {
+                alias: 'base',
+                type: 'number',
+                demandOption: 'true'
+            })
+            .check((argv, option) => { 
+                if(isNaN(argv.b)){
+                    throw 'La base tiene que ser un numero'
+                }
+                return true;
+             })  
+             .option('l', {
+                alias: 'listar',
+                type: 'boolean',
+                demandOption: 'true',
+                default: false
+            })
+                            
+            .argv;
+
 console.clear();
 
-console.log(process.argv);
+// console.log(process.argv);
 console.log(argv)
 
-console.log('base: yargs', argv.base)
+// console.log('base: yargs', argv.base)
 
 // const base= 3;
 
-// crearArchivo(base)
-// .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-// .catch(err => console.log(err))
+crearArchivo(argv.b, argv.l)
+.then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+.catch(err => console.log(err))
         
--- 03.1-bases-Console/helpers/multiplicar.js
+++ 03.1-bases-Console/helpers/multiplicar.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 
-    const crearArchivo = async(base = 5) => { 
+    const crearArchivo = async(base = 5, l = false) => { 
 
         try {
-            
-            console.log('----------------------')
-            console.log(`    Tabla del: ${base}`)
-            console.log('----------------------')
-    
+          
             let salida = '';
     
             for (let i = 1; i < 11; i++) {
                 salida += ` ${base} * ${i} = ${base*i}\n`;            
             }
-            console.log(salida);
+            if (l) {
+                console.log('----------------------')
+                console.log(`    Tabla del: ${base}`)
+                console.log('----------------------')
+                console.log(salida)
+               }
     
            fs.writeFileSync( `tabla-${base}.txt`, salida);           
     
