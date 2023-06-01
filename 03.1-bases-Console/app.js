fa8544d412515fa109e9e6f85cdfec76f4c1a528
edcastillob <edwar.castillo@gmail.com>
2023-06-01 09:59:14 -0400

    Lib-yargs

---
+++ 03.1-bases-Console/app.js
@@ -0,0 +1,15 @@
+const { crearArchivo } = require('./helpers/multiplicar')
+const argv = require('yargs').argv;
+console.clear();
+
+console.log(process.argv);
+console.log(argv)
+
+console.log('base: yargs', argv.base)
+
+// const base= 3;
+
+// crearArchivo(base)
+// .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+// .catch(err => console.log(err))
+        
