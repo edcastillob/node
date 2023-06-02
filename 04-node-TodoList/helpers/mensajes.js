8364c9de630fe334b55fb19a2da92e615bab779e
edcastillob <edwar.castillo@gmail.com>
2023-06-01 21:20:21 -0400

    TodoList Node

---
+++ 04-node-TodoList/helpers/mensajes.js
@@ -0,0 +1,51 @@
+require('colors');
+
+const mostrarMenu = () => { 
+
+    console.clear();
+
+    console.log('-- TodoList @Edwar Castillo--'.blue);
+    console.log('==========================='.blue);
+    console.log('   Seleccione una Opción   '.blue);
+    console.log('===========================\n'.blue);
+    
+    console.log(`${ '1.'.blue } Crear tarea`);
+    console.log(`${ '2.'.blue } Listar tareas`);
+    console.log(`${ '3.'.blue } Tareas completadas`);
+    console.log(`${ '4.'.blue } Tareas pendientes`);
+    console.log(`${ '5.'.blue } Completar tareas`);
+    console.log(`${ '6.'.blue } Borrar tareas`);
+    console.log(`${ '7.'.blue } Salir\n`);
+
+
+    const readline = require('readline').createInterface({ // lectura de la opcion readline uso propiedades del process
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question('Seleccione una opción: ', (opt) => { 
+       
+        readline.close();
+     })
+
+ }
+
+    const pausa = () => { 
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question(`\nPresione ${ 'Enter'.yellow } para continuar...\n`, (opt) => { 
+           
+            readline.close();
+         })
+    
+}
+    
+
+
+ module.exports = {
+    mostrarMenu,
+    pausa
+ }
